cc0bac00b04316556fe89a38e6adc7682a366461
Pranav <pranav.shastri04@gmail.com>
2023-10-06 15:57:15 +0530

    yay

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -80,7 +80,7 @@ const loginUser = asyncHandler(async (req, res) => {
     }
   } else {
     res.status(400);
-    throw new Error("email or password is not valid");
+    throw new Error("email or password is not valid"); //yay
 
   }
 });
